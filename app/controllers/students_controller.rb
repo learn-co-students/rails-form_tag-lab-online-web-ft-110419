0601dc7e27b3e8fde7a5113c428c579097db2f3b
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-28 06:15:00 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,12 @@ class StudentsController < ApplicationController
   end
   
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to "index"
+    student = Student.create(
+      first_name: params[:student][:first_name],
+      last_name: params[:student][:last_name]
+    )
+
+    redirect_to student_path(student)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :new]
-  get '/students/:id', to: 'students#show', as: 'student'
-  get '/student/create', to: 'students#create', :as =>'create_path'
+ resources :students, only: [:index, :new, :create, :show]
 end
